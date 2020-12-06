baf9ac963bb3ed09f3f8efacda1746d6d64168f7
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2020-12-06 21:10:36 +0200

    Exclude dash from regex

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -9,17 +9,20 @@ const input: PasswordRule[] = prepareInput(readInput())
     .map(password => {
         // Example: 15-16 l: klfbblslvjclmlnqklvg
         return {
-            min: Number(password.match(/^\d+-/)[0]),
+            min: Number(password.match(/^\d+(?=-)/)[0]),
             max: Number(password.match(/(\d+)(?=\s)/)[0]),
-            sym: password.match(/([a-z])(?=\:)/)[0],
-            password: password.match(/(?<=\: )(\w*)/)[0]
+            sym: password.match(/([a-z])(?=:)/)[0],
+            password: password.match(/(?<=: )(\w*)/)[0]
         }
     })
 
 const goA = (input: PasswordRule[]) => {
     let counter = 0;
     for (const pw of input) {
+
+
         const {min, max, sym, password} = pw;
+        console.log(min, max, sym, password);
         const count = (password.match(`/${sym}/g`) || []).length;
         if (count >= min && count <= max) counter++;
     }
