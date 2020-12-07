2743d02e2af02b5dde1fd0a661edd8da0aa3c50e
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2020-12-07 17:44:30 +0200

    Match regex with key

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -17,21 +17,36 @@ const goA = (input: string[]) => {
 const goB = (input: string[]) => {
     let count = 0;
 
-    const validationRules = {
-        byr: new RegExp(/(19[2-9]\d)|200[0-2]/),
-        iyr: new RegExp(/20(1[0-9]|20)/),
-        eyr: new RegExp(/20(2[0-9]|30)/),
-        hgt: new RegExp(/(1([5-8][0-9]|9[0-3])cm)|((59|6[0-9]|7[0-6])in)/),
-        hcl: new RegExp(/#[0-9,a-f]{6}/),
-        ecl: new RegExp(/amb|blu|brn|gry|grn|hzl|oth/),
-        pid: new RegExp(/\d{9}/),
+    // byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    // hgt (Height) - a number followed by either cm or in:
+    // If cm, the number must be at least 150 and at most 193.
+    // If in, the number must be at least 59 and at most 76.
+    // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    // pid (Passport ID) - a nine-digit number, including leading zeroes.
+    // cid (Country ID) - ignored, missing or not.
+
+    const validationRules: {[key: string]: string} = {
+        byr: '(19[2-9]\d)|200[0-2]',
+        iyr: '20(1[0-9]|20)',
+        eyr: '20(2[0-9]|30)',
+        hgt: '(1([5-8][0-9]|9[0-3])cm)|((59|6[0-9]|7[0-6])in)',
+        hcl: '#[0-9,a-f]{6}',
+        ecl: 'amb|blu|brn|gry|grn|hzl|oth',
+        pid: '\d{9}',
     }
+
+    console.log()
 
     for (const pass of input) {
         if (fields.every(field =>
-            pass.indexOf(field) !== -1 && validationRules[field].test(pass)
-        )) count++;
+            pass.indexOf(field) !== -1 && new RegExp(`${fields[0]}:${validationRules[fields[0]]}`).test(pass)
+        ))
+            count++;
     }
+
     return count;
 }
 
