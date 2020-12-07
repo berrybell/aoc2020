be1221e16c11c56a5b0e320c5681269a198c7485
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2020-12-07 16:27:46 +0200

    Add test data for Day 4.2

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -14,11 +14,38 @@ const goA = (input: string[]) => {
     return count;
 }
 
-const goB = (input) => {
+const goB = (input: string[]) => {
     return
 }
 
 /* Tests */
+
+const invalidPasses = "eyr:1972 cid:100\n" +
+    "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\n" +
+    "\n" +
+    "iyr:2019\n" +
+    "hcl:#602927 eyr:1967 hgt:170cm\n" +
+    "ecl:grn pid:012533040 byr:1946\n" +
+    "\n" +
+    "hcl:dab227 iyr:2012\n" +
+    "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\n" +
+    "\n" +
+    "hgt:59cm ecl:zzz\n" +
+    "eyr:2038 hcl:74454a iyr:2023\n" +
+    "pid:3556412378 byr:2007";
+
+const validPasses = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\n" +
+    "hcl:#623a2f\n" +
+    "\n" +
+    "eyr:2029 ecl:blu cid:129 byr:1989\n" +
+    "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\n" +
+    "\n" +
+    "hcl:#888785\n" +
+    "hgt:164cm byr:2001 iyr:2015 cid:88\n" +
+    "pid:545766238 ecl:hzl\n" +
+    "eyr:2022\n" +
+    "\n" +
+    "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
 
 test(goA(prepareInput("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n" +
     "byr:1937 iyr:2017 cid:147 hgt:183cm\n" +
@@ -33,6 +60,9 @@ test(goA(prepareInput("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n" +
     "\n" +
     "hcl:#cfa07d eyr:2025 pid:166559648\n" +
     "iyr:2011 ecl:brn hgt:59in")), 2);
+
+test(goB(prepareInput(invalidPasses)), 0);
+test(goB(prepareInput(validPasses)), 4);
 
 /* Results */
 
